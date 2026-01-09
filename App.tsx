8044641134547ef7022836b97320c4f64200c8e2
Amine Ben Yedder <aminebenyedder@outlook.com>
2026-01-09 15:36:33 +0100

    Assets

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { DotLottie } from "@lottiefiles/dotlottie-react-native";
+import { Dimensions, View } from "react-native";
+import Background from "./assets/Background";
+import Flag from "./assets/Flag";
+
+const BACKGROUND_ASPECT_RATIO = 665 / 241;
+const GROUND_LEVEL = 10;
+const MONSTER_SIZE = 100;
+const MONSTER_INITIAL_POSITION = 25;
+const FLAG_SIZE = 75;
+const FLAG_OFFSET = 50;
+const CHARACTER_SIZE = 75;
+const CHARACTER_INITIAL_POSITION = 225;
 
 export default function App() {
+  const { height } = Dimensions.get("window");
+
+  const backgroundHeight = height;
+  const backgroundWidth = height * BACKGROUND_ASPECT_RATIO;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
+      <Background width={backgroundWidth} height={backgroundHeight} />
+      <View
+        style={{
+          position: "absolute",
+          bottom: GROUND_LEVEL,
+          left: MONSTER_INITIAL_POSITION,
+        }}
+      >
+        <DotLottie
+          source={require("./assets/monster.lottie")}
+          style={{
+            width: MONSTER_SIZE,
+            height: MONSTER_SIZE,
+          }}
+          loop
+          autoplay
+        />
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          bottom: GROUND_LEVEL,
+          right: FLAG_OFFSET,
+        }}
+      >
+        <Flag width={FLAG_SIZE} height={FLAG_SIZE} />
+      </View>
+
+      <View
+        style={{
+          position: "absolute",
+          bottom: GROUND_LEVEL,
+          left: CHARACTER_INITIAL_POSITION,
+        }}
+      >
+        <DotLottie
+          source={require("./assets/character.lottie")}
+          style={{
+            width: CHARACTER_SIZE,
+            height: CHARACTER_SIZE,
+          }}
+          loop
+          autoplay
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
